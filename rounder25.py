0f72585b479f7f8efca8358127542a921de83469
Dave Wade-Stein <dws@DWS-MacBook.local>
2016-01-20 10:25:14 -0800

    Fixing

--- rounder25.py
+++ rounder25.py
@@ -16,25 +16,3 @@ def rounder25(amount):
 
     return amount
 
-import unittest
-
-class TestRounder25(unittest.TestCase):
-
-  def setUp(self):
-      print("\nSetting up a test.")
-
-  def tearDown(self):
-      print("Tearing down a test.")
-
-  def test_roundup(self):
-      self.assertEqual(rounder25(1.03), 1.25)
-      self.assertEqual(rounder25(1.89), 2.00)
-
-  def test_noround(self):
-      self.assertEqual(rounder25(1.00), 1.00)
-      self.assertEqual(rounder25(1.25), 1.25)
-      self.assertEqual(rounder25(1.50), 1.50)
-      self.assertEqual(rounder25(1.75), 1.75)
-
-if __name__ == '__main__':
-    unittest.main()
